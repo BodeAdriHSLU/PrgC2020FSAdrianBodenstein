67453197966a566c0575a0223ffdf8b490160d0d
ulutuai <adrian.bodenstein@gmail.com>
2020-04-29 16:18:28 +0200

    adding MSP430 Folder

---
+++ 02_MSP430/Blink/Debug/blink.d
@@ -0,0 +1,15 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: /home/adi/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h
+blink.obj: /home/adi/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr4133.h
+blink.obj: /home/adi/ti/ccs930/ccs/ccs_base/msp430/include/in430.h
+blink.obj: /home/adi/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.5.LTS/include/intrinsics.h
+blink.obj: /home/adi/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.5.LTS/include/intrinsics_legacy_undefs.h
+
+../blink.c: 
+/home/adi/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h: 
+/home/adi/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr4133.h: 
+/home/adi/ti/ccs930/ccs/ccs_base/msp430/include/in430.h: 
+/home/adi/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.5.LTS/include/intrinsics.h: 
+/home/adi/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.5.LTS/include/intrinsics_legacy_undefs.h: 
---
+++ 02_MSP430/OutOfBox/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibc.a -l"/home/adi/PrgC2020FSAdrianBodenstein/02_MSP430/DriverLib/Debug/DriverLib.lib"
+
